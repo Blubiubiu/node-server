3ca220053068222f8eee3aa4fb804e2583477173
Bali <lisiyuan@tengyue360.com>
2019-08-02 11:19:27 +0800

    v1.0.0

--- config/config.default.js
+++ config/config.default.js
@@ -6,25 +6,33 @@
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
-  /**
-   * built-in config
-   * @type {Egg.EggAppConfig}
-   **/
-  const config = exports = {};
+    /**
+     * built-in config
+     * @type {Egg.EggAppConfig}
+     **/
+    const config = exports = {};
 
-  // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1564578069537_9716';
+    // use for cookie sign key, should change to your own and keep security
+    config.keys = appInfo.name + '_1564578069537_9716';
 
-  // add your middleware config here
-  config.middleware = [];
+    // add your middleware config here
+    config.middleware = [];
 
-  // add your user config here
-  const userConfig = {
-    // myAppName: 'egg',
-  };
+    // add your user config here
+    const userConfig = {
+        // myAppName: 'egg',
+    };
 
-  return {
-    ...config,
-    ...userConfig,
-  };
+    return {
+        ...config,
+        ...userConfig,
+    };
 };
+
+exports.cluster = {
+    listen: {
+        port: 7000,
+        hostname: '39.107.96.198',
+        // path: '/var/run/egg.sock',
+    }
+}
