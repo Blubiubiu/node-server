2843708076b0e3dbdd58b7fed9cf71a9553fa882
Bali <lisiyuan@tengyue360.com>
2019-08-07 18:02:07 +0800

    v1.0.0

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class UserController extends Controller {
     async info() {
         const { ctx } = this;
-        const userId = ctx.params.id;
+        const userId = ctx.request.body.id;
         const data = await ctx.service.user.find(userId);
         ctx.body = data;
     }
